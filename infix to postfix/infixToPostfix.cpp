57f92a56e2b62e34a4df69091a05fa5968de1b45
Harshit <62823486+harshit7962@users.noreply.github.com>
2022-06-06 14:35:44 +0530

    Update infixToPostfix.cpp

--- infix to postfix/infixToPostfix.cpp
+++ infix to postfix/infixToPostfix.cpp
@@ -1,41 +1,40 @@
-bool higherPrec(char c1, char c2) {
-    if(c1=='^' && c2=='^') return false;
+//To check if precedence of two operators
+bool higherprec(char c1, char c2) {
+    if(c2=='(') return true;
     else if(c1=='^') return true;
-    else if((c1=='/' || c1=='*') && (c2=='/' || c2=='*')) return false;
-    else if(c1=='/' || c1=='*') return true;
-    
+    else if(c2=='^') return false;
+    else if((c1=='/'||c1=='*') && (c2=='/'||c2=='*')) return false;
+    else if(c1=='/'|| c1=='*') return true;
     return false;
-    
 }
 
 string infixToPostfix(string s) {
-    string res="";
     stack<char> st;
     
-    for(char c:s) {
-        if(c>='a' && c<='z') res+=c;
-        else {
-            if(c=='(') st.push('(');
-            else if(c==')') {
-                while(st.top()!='(') {res+=st.top(); st.pop();}
+    string res ="";
+    
+    for(char c:s){
+        if((c>='a'&&c<='z') || (c>='A' && c<='Z')) res+=c;
+        else if(c=='(') {st.push(c);}
+        else if(c==')') {
+            while(st.top()!='(') {
+                res+=st.top();
                 st.pop();
-            } else {
-                if(st.empty()) st.push(c);
-                else {
-                    if(st.top()=='('|| higherPrec(c, st.top())) st.push(c);
-                    else if(!higherPrec(c, st.top())) {
-                        while(!st.empty() && !higherPrec(c, st.top())) {
-                            res+=st.top();
-                            st.pop();
-                        }
-                        st.push(c);
-                    }
+            }
+            st.pop();
+        } else {
+            if(st.empty()) st.push(c);
+            else {
+                while(!st.empty() && !higherprec(c, st.top())) {
+                    res+=st.top();
+                    st.pop();
                 }
+                st.push(c);
             }
         }
     }
     
-    while(!st.empty()) {res+=st.top(); st.pop();}
+    while(!st.empty()) {res+=st.top();st.pop();};
     
     return res;
 }
